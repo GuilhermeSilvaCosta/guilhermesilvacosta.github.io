b5867d20ee7d2a54520e4f5f62f7f4d2de74a410
Guilherme <tec.guilherme.dsc@gmail.com>
2021-04-16 17:05:55 -0300

    Update portfolio

---
+++ precache-manifest.6f48187f85e973f366bae43fa4c25db5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c95999dda26912e953046a722babb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8768a69f2bd01dd56a65",
+    "url": "/static/css/3.ac39bc07.chunk.css"
+  },
+  {
+    "revision": "126d0d7a038516fd7f4b",
+    "url": "/static/css/4.8381c7f3.chunk.css"
+  },
+  {
+    "revision": "24554ea93d6e3f14065f",
+    "url": "/static/css/6.97f17090.chunk.css"
+  },
+  {
+    "revision": "c47f2575839a1e97641e",
+    "url": "/static/css/7.ad6ef01b.chunk.css"
+  },
+  {
+    "revision": "8bd0c2c4c7dc9cfba994",
+    "url": "/static/css/main.95254035.chunk.css"
+  },
+  {
+    "revision": "463eea10e303fdd5531f",
+    "url": "/static/js/2.cb2d7a4a.chunk.js"
+  },
+  {
+    "revision": "f0b6cee782656d3f62c5d2f923ca0b1a",
+    "url": "/static/js/2.cb2d7a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8768a69f2bd01dd56a65",
+    "url": "/static/js/3.534a9811.chunk.js"
+  },
+  {
+    "revision": "126d0d7a038516fd7f4b",
+    "url": "/static/js/4.5df6f9bb.chunk.js"
+  },
+  {
+    "revision": "2ddd8874c70c20cbe203",
+    "url": "/static/js/5.069dc127.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/5.069dc127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24554ea93d6e3f14065f",
+    "url": "/static/js/6.1c05ef6b.chunk.js"
+  },
+  {
+    "revision": "c47f2575839a1e97641e",
+    "url": "/static/js/7.5b7bae56.chunk.js"
+  },
+  {
+    "revision": "8bd0c2c4c7dc9cfba994",
+    "url": "/static/js/main.febeb909.chunk.js"
+  },
+  {
+    "revision": "7be7d83b22d1fcf0b58b",
+    "url": "/static/js/runtime-main.96b78d23.js"
+  }
+]);
